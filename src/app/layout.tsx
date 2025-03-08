0e6fd044ada5155e8b21d7968f7172c059d3d76f
Bona3121 <marlamarlena999@gmail.com>
2025-03-08 23:25:01 +0700

    unsolve navbar problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
+
+
+import React from "react";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/common/navbar";
 import Footer from "@/components/common/footer";
+
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -29,3 +33,21 @@ export default function RootLayout({
     </html>
   );
 }
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{ children: React.ReactNode }>) {
+//   const pathname = usePathname();
+//   const hideLayout = pathname === '/auth' || pathname === '/auth/register';
+
+//   return (
+//     <html lang="en">
+//       <body className={`${poppins.variable} antialiased`}>
+//         {!hideLayout && <Navbar />}
+//         {children}
+//         {!hideLayout && <Footer />}
+//       </body>
+//     </html>
+//   );
+// }
+
